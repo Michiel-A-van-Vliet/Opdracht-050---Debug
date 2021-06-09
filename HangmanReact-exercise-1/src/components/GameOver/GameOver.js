b64d8ee82f2f9a87e1f1badb2ad743d42b914210
Michiel van Vliet <Michiel.Arie.van.Vliet@GMail.com>
2021-06-09 08:54:43 +0200

    Opdracht 1 opgelost (debug)

--- HangmanReact-exercise-1/src/components/GameOver/GameOver.js
+++ HangmanReact-exercise-1/src/components/GameOver/GameOver.js
@@ -1,19 +1,23 @@
 import React from "react";
-import win from "../../assets/win.gif";
-import lose from "../../assets/lose.gif";
+// import win from "../../assets/win.gif";
+// import lose from "../../assets/lose.gif";
 
-const GameOver = props => {
+const GameOver = (props) => {
+  console.log("* GameOver");
+  console.log("- props:", props);
+  console.log("- chosenWord:", props.chosenWord);
+  console.log("- wordGuessed:", props.wordGuessed);
   const winResult = (
     <div className="win">
       <h2>You won!</h2>
-      <img src={win} alt="win" />
+      {/* <img src={win} alt="win" /> */}
     </div>
   );
 
   const loseResult = (
     <div className="lose">
       <h2>You lost..</h2>
-      <img src={lose} alt="lose" />
+      {/* <img src={lose} alt="lose" /> */}
       <p>The chosen word was: {props.chosenWord}</p>
     </div>
   );
